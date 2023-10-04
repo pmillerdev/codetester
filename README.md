ee3e6b5c33773a94c5ab1ef7740bf7f8074a04f4
pmillerdev <pmillerdev@gmail.com>
2023-10-04 12:10:53 +0100

    tidy up and update readme

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Codetester
 
-An app that allows the user to test the output of their HTML, CSS and JavaScript code (my version of CodePen).
+My version of Code Pen - an app that allows the user to test the output of their HTML, CSS and JavaScript code.
 
 Technologies used: HTML, CSS, JavaScript, jQuery.
+
+## Running Locally
+
+Open `index.html` in a browser window.
--- index.js
+++ index.js
@@ -3,47 +3,60 @@ Most of the interactivity was created using jQuery.*/
 
 //Function that updates the output iframe based on what is inside the text areas.
 function updateOutput() {
-    //Update for the html and css.
-    $("iframe").contents().find("html").html("<html><head><style type='text/css'>"
-    + $("#cssPanel").val() + "</style></head><body>" + $("#htmlPanel").val() + "</body></html>");
-    //Update for the javascript.
-    document.getElementById("outputPanel").contentWindow.eval($("#javascriptPanel").val());
+  //Update for the html and css.
+  $("iframe")
+    .contents()
+    .find("html")
+    .html(
+      "<html><head><style type='text/css'>" +
+        $("#cssPanel").val() +
+        "</style></head><body>" +
+        $("#htmlPanel").val() +
+        "</body></html>"
+    );
+  //Update for the javascript.
+  document
+    .getElementById("outputPanel")
+    .contentWindow.eval($("#javascriptPanel").val());
 }
 
 //Hover function for the buttons.
-$(".toggleButton").hover(function() {
-//Add the highlightedButton class to the top buttons when they are hovered over.
+$(".toggleButton").hover(
+  function () {
+    //Add the highlightedButton class to the top buttons when they are hovered over.
     $(this).addClass("highlightedButton");
-//Removes the highlightedButton class when the buttons are no longer hovered over.
-}, function() {
+    //Removes the highlightedButton class when the buttons are no longer hovered over.
+  },
+  function () {
     $(this).removeClass("highlightedButton");
-});
+  }
+);
 
 //On-click function for the buttons.
-$(".toggleButton").click(function() {
-    //Make the button active.
-    $(this).toggleClass("active");
-    //Remove the highlightedButton class.
-    $(this).removeClass("highlightedButton");
-    //Create a variable to store the panel ID.
-    var panelId = $(this).attr("id") + "Panel";
-    //Select the panel ID and make the panel appear.
-    $("#" + panelId).toggleClass("hidden");
-    //Variable to store the number of panels activated by the user.
-    var numberOfActivePanels = 4 - $('.hidden').length;
-    //Define the width of each panel based on the number of active panels.
-    $(".panel").width(($(window).width() / numberOfActivePanels) - 10);
-})
+$(".toggleButton").click(function () {
+  //Make the button active.
+  $(this).toggleClass("active");
+  //Remove the highlightedButton class.
+  $(this).removeClass("highlightedButton");
+  //Create a variable to store the panel ID.
+  var panelId = $(this).attr("id") + "Panel";
+  //Select the panel ID and make the panel appear.
+  $("#" + panelId).toggleClass("hidden");
+  //Variable to store the number of panels activated by the user.
+  var numberOfActivePanels = 4 - $(".hidden").length;
+  //Define the width of each panel based on the number of active panels.
+  $(".panel").width($(window).width() / numberOfActivePanels - 10);
+});
 
 //Alter the window height, ensuring it is below the header.
 $(".panel").height($(window).height() - $("#header").height() - 15);
 //Alter the window width.
-$(".panel").width(($(window).width() / 2) - 10);
+$(".panel").width($(window).width() / 2 - 10);
 
 //Run the update output function.
 updateOutput();
 
 //Update the output as users change the content of the panels.
-$("textarea").on('change keyup paste', function() {
-    updateOutput();
+$("textarea").on("change keyup paste", function () {
+  updateOutput();
 });
